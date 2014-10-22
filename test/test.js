504ecf72652968562ffa54d28c2fc750402af588
Daniel Kleehammer <dkleehammer@gmail.com>
2014-10-22 16:47:01 -0500

    Attempting to get providers working

--- test/test.js
+++ test/test.js
@@ -1,32 +1,79 @@
 
+var provider = function(name, Constructor) {
+    var _this = this, providerName = name + 'Provider';
+
+    // if no Constructor then return the singleton
+    if (!Constructor) {
+        return _this.providers[providerName] || undefined;
+    }
+
+    // check if the provider we are creating already exists
+    if (_this.providers[providerName]) {
+        throw new Error('Provider ' + name + ' already exists');
+    }
+
+    // create the instance (set the instance into the providers object so it is cache as constructed)
+    return _this.providers[providerName] = new Constructor();
+}
+
+var factory = function factory(name, Factory) {
+    var _this = this;
+
+    return provider.call(_this, name, function GenericProvider() {
+        this.$get = Factory;
+    });
+};
+
+// var mapContainer = function mapContainer(key) {
+//     return this.container[key];
+// };
+
+// var service = function service(name, Service) {
+//     var deps = arguments.length > 2 ? slice.call(arguments, 1) : null,
+//         _this = this;
+
+//     return factory.call(carbon.container, name, function GenericFactory() {
+//         if (deps) {
+//             Service = Service.bind.apply(Service, deps.map(mapContainer, carbon.container));
+//         }
+//         return new Service();
+//     });
+// };
+
 var carbon = {
     instances: {},
-    get: function(name) {
-        if (this.instances[name]) {
-            return this.instances[name];
-        }
-    },
-    application: function(name, configuration, start) {
-        try {
-            if (this.instances[name]) {
-                throw new Error('Application instance ' + name + ' already exists.');
-            }
+    create: function() {
+        // create a new instance
+        var instance = Object.create(null);  // make sure it works for >=IE9
+        instance.providers = {};
+        instance.provider = provider;
 
-            this.instances[name] = {configuration: configuration, start: start};  // ? need to create an instance so we can store services and factories in it
-        } catch (e) {
-            console.error(e);
-        }
+        instance.factory = factory;
+        // instance.service = service;
+        return this.instances[name] = instance;
+    },
+    get: function() {
+        return this.instances[name];
     }
 };
 
+// create app
+var app = carbon.create('MyApp');
 
-var app = carbon.application('Test', {}, function() {
-    console.log('name');
+// create 
+var p = app.provider('Test', function() {
+    this.$get = function() {
+
+    }
 });
 
-var app2 = carbon.get('Test');
 
-console.log(app2);
+var f = app.factory('Test', function() {
+    var x = function()  {
+        console.log('in x');
+    }
 
-// Service Example
+    return new x();
+});
 
+console.log(f.$get());
