b50a16e6f2273f147a2c7d910cfe1c515e76e700
Daniel Kleehammer <dkleehammer@gmail.com>
2014-10-23 12:54:55 -0500

    Completed initial dependency injection system.

---
+++ Gulpfile.js
@@ -0,0 +1,45 @@
+
+var gulp = require('gulp'),
+    bower = require('./bower.json'),
+    pkg = require('./package.json'),
+    tasks = require('gulp-load-plugins')();
+
+
+// ----------------------------------------
+// header
+// ----------------------------------------
+var header = ['/**',
+  ' * <%= pkg.name %> - <%= pkg.description %>',
+  ' * @version v<%= pkg.version %>',
+  ' */',
+  ''].join('\n');
+
+
+// ----------------------------------------
+// jshint (js lint the file)
+// ----------------------------------------
+gulp.task('lint', function() {
+    return gulp.src(['./lib/src/**/*.js'])
+        .pipe(tasks.jshint())
+        .pipe(tasks.jshint.reporter('default'));
+});
+
+
+// ----------------------------------------
+// js
+// ----------------------------------------
+gulp.task('js', ['lint'], function() {
+    gulp.src(['./lib/vendor/**/*.js', './lib/src/**/*.js'])
+        .pipe(tasks.sourcemaps.init())
+        .pipe(tasks.concat(pkg.name + '.min.js'))
+        .pipe(tasks.uglify())
+        .pipe(tasks.header(header, {pkg: pkg}))
+        .pipe(tasks.sourcemaps.write())
+        .pipe(gulp.dest('./dist/'));
+});
+
+
+// ----------------------------------------
+// user tasks
+// ----------------------------------------
+gulp.task('default', ['js']);
--- README.md
+++ README.md
@@ -1,11 +1,2 @@
 ### carbon
 ======
-
-###### Solar System Exploration, 1950s – 1960s
-
-- [ ] service - return instances that need to be constructed (e.g new service())
-- [ ] factory - return a singleton already instantiated service
-
-
-we may need a provider.. ?
-http://stackoverflow.com/questions/15666048/service-vs-provider-vs-factory
