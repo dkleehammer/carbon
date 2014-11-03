1a09d3fa88345a986ce9460ba10b43045db79650
Daniel Kleehammer <dkleehammer@gmail.com>
2014-11-03 17:19:28 -0600

    Done with carbon2 changes and merged into carbon.  Need to finalize testing.

--- examples/single-page-app/static/js/single-page-app.js
+++ examples/single-page-app/static/js/single-page-app.js
@@ -1,25 +1,57 @@
 
 var m1 = carbon.module('M1');
-console.log(m1);
 
-m1.service('First', function() {
+var f1 = m1.service('First', function() {
     this.count = 0;
     this.message = function(msg) {
         console.log(msg);
+    };
+    this.add = function(num) {
+        this.count += num;
+    };
+    this.get = function() {
+        return this.count;
     };
 });
 
 var second = m1.service('Second', function(first, jq) {
     this.message = function(msg) {
         first.message(msg);
-
-        console.log('jquery in Second: ', jq);
-    };
-}, 'First', jQuery);
+        jq('#test').append('here by using jquery as dependency into second service');
+    };  
+}, 'First', $);
 
 second.message('chained message testing dependency injection');
 
+var f2 = m1.service('First');
 
+console.log('f1 & f2 counts, testing if count is same: f1: ', f1.get(), ' | f2: ', f2.get());
+f1.add(10);
+console.log('f1 & f2 counts, testing if count is still the same instance: f1: ', f1.get(), ' | f2: ', f2.get());
+
+var third = m1.factory('Third', function(jq, first, second) {
+    this.count = 0;
+    this.add = function(num) {
+        this.count += num;
+    };
+    this.get = function() {
+        return this.count;
+    };
+}, $, 'First', 'Second');
+
+var fourth = m1.factory('Fourth', function(jq, first, second, third) {
+    this.count = 0;
+    this.add = function(num) {
+        this.count += num;
+    };
+    this.get = function() {
+        return this.count;
+    };
+}, $, 'First', 'Second', 'Third');
+
+console.log('Third & Fourth counts, testing if count is same: Third: ', third.get(), ' | Fourth: ', fourth.get());
+fourth.add(10);
+console.log('Third & Fourth counts, testing if count is different: Third: ', third.get(), ' | Fourth: ', fourth.get());
 
 /*
 // ----------------------------------------
